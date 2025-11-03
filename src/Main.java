36b7adf763267a9d546e99f815442e650f181506
Natasha Farias <nataschafarias@gmail.com>
2025-11-03 04:18:07 +0000

    Edit Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main (String[] args) {
+
+        System.out.println("5ft, 8in = " + convertToCentimeters(5,8) + "cm");
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+    }
+
+    public static double convertToCentimeters(int inches) {
+
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+
+        //return convertToCentimeters((feet * 12) + inches);
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        double result  = convertToCentimeters(totalInches);
+        return result;
     }
 }
